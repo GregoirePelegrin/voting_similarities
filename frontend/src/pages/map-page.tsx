7bc4267b3ed340f617426f93762fabfe76b72208
GregoirePelegrin <pelegrin.gregoire@protonmail.com>
2026-06-30 14:03:56 +0200

    handling loading main screen

--- frontend/src/pages/map-page.tsx
+++ frontend/src/pages/map-page.tsx
@@ -1,43 +1,52 @@
-import React, { useEffect } from "react";
-import { Box } from "@mui/material";
-import { observer } from "mobx-react-lite";
+import React, {useEffect} from "react";
+import {Box} from "@mui/material";
+import {observer} from "mobx-react-lite";
 import rootStore from "../stores/root-store";
 import AnimatedPage from "../components/shared/animated-page";
+import {CardSkeleton} from "../components/shared/loading-skeleton";
 import CategoryFilter from "../components/shared/category-filter";
 import PeopleScatter from "../components/map/people-scatter";
 import GroupsScatter from "../components/map/groups-scatter";
 import MethodologyPanel from "../components/map/methodology-panel";
 
 const MapPage: React.FC = observer(() => {
-  const { embeddings, ui } = rootStore;
+    const {embeddings, ui} = rootStore;
 
-  useEffect(() => {
-    embeddings.fetchPeopleEmbedding(ui.selectedCategory);
-    embeddings.fetchGroupsEmbedding(ui.selectedCategory);
-  }, [ui.selectedCategory]);
+    useEffect(() => {
+        embeddings.fetchPeopleEmbedding(ui.selectedCategory);
+        embeddings.fetchGroupsEmbedding(ui.selectedCategory);
+    }, [ui.selectedCategory]);
 
-  return (
-    <AnimatedPage>
-      <Box sx={{ mb: 2 }}>
-        <CategoryFilter />
-      </Box>
-      <MethodologyPanel />
-      {embeddings.peopleEmbedding && (
-        <PeopleScatter
-          points={embeddings.peopleEmbedding.points}
-          barycenters={embeddings.peopleEmbedding.barycenters}
-          stress={embeddings.peopleEmbedding.stress}
-        />
-      )}
-      <Box sx={{ mt: 3 }} />
-      {embeddings.groupsEmbedding && (
-        <GroupsScatter
-          points={embeddings.groupsEmbedding.points}
-          stress={embeddings.groupsEmbedding.stress}
-        />
-      )}
-    </AnimatedPage>
-  );
+    const isLoading = !embeddings.peopleEmbedding && !embeddings.groupsEmbedding;
+
+    return (
+        <AnimatedPage>
+            <Box sx={{mb: 2}}>
+                <CategoryFilter/>
+            </Box>
+            <MethodologyPanel/>
+            {isLoading ? (
+                <CardSkeleton/>
+            ) : (
+                <>
+                    {embeddings.peopleEmbedding && (
+                        <PeopleScatter
+                            points={embeddings.peopleEmbedding.points}
+                            barycenters={embeddings.peopleEmbedding.barycenters}
+                            stress={embeddings.peopleEmbedding.stress}
+                        />
+                    )}
+                    <Box sx={{mt: 3}}/>
+                    {embeddings.groupsEmbedding && (
+                        <GroupsScatter
+                            points={embeddings.groupsEmbedding.points}
+                            stress={embeddings.groupsEmbedding.stress}
+                        />
+                    )}
+                </>
+            )}
+        </AnimatedPage>
+    );
 });
 
 export default MapPage;
